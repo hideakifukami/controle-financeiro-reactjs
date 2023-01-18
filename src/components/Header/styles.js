9b15d8a57b0e8c52580b315709ce36aaf38fadfe
Hideaki Fukami <denys.fukami@gmail.com>
2023-01-18 17:38:50 -0300

    Create Header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+    height: 150px;
+    text-align: center;
+    background: teal;
+`
+export const Header = styled.div``
+
+export const Title = styled.h1`
+    padding-top: 20px;
+    color: #fff;
+`
---
+++ src/components/Resume/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as C from './styles'
+
+const Resume = () => {
+  return (
+    <C.Container>
+        Teste
+    </C.Container>
+  )
+}
+
+export default Resume
