385053c8ca2c8c803c195c01a7466704860f6ecf
Hideaki Fukami <denys.fukami@gmail.com>
2023-01-18 19:13:00 -0300

    Create Resume Item

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import ResumeItem from '../ResumeItem'
 import * as C from './styles'
+import {
+    FaRegArrowAltCircleUp,
+    FaRegArrowAltCircleDown,
+    FaDollarSign,
+  } from "react-icons/fa"
 
 const Resume = () => {
   return (
     <C.Container>
-        Teste
+        <ResumeItem 
+            title="Entradas"
+            Icon={FaRegArrowAltCircleUp}
+            value="10000"
+        />
+        <ResumeItem 
+            title="Saídas"
+            Icon={FaRegArrowAltCircleDown}
+            value="10000"
+        />
+        <ResumeItem 
+            title="Total"
+            Icon={FaDollarSign}
+            value="10000"
+        />
     </C.Container>
   )
 }
