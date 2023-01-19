8b58a0d9d726fae554789c93bc5a6a700cdac3af
Hideaki Fukami <denys.fukami@gmail.com>
2023-01-18 21:39:41 -0300

    Form Interative

---
+++ src/components/Grid/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import GridItem from '../GridItem'
+import * as C from './styles'
+
+const Grid = () => {
+  return (
+    <>
+        <div>Grid</div>
+        <GridItem />
+    </>
+  )
+}
+
+export default Grid
---
+++ src/components/GridItem/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as C from './styles'
+
+const GridItem = () => {
+  return (
+    <div>GridItem</div>
+  )
+}
+
+export default GridItem
